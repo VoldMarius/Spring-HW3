f3e64e604457370134f4cba6a04540c3ed092ad6
VoldMarius <ukusil@mail.ru>
2024-02-15 11:56:06 +0300

    Started HomeWork 3

--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -1,11 +1,8 @@
 package com.example.service;
 
 import com.example.domain.User;
-import com.example.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 
 @Service
